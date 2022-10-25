4a5fd70baac8f29c5cf6b5366aad3c78f9b6f3ff
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-10-25 14:24:50 -0700

    file syntax practice

--- myscript.sh
+++ myscript.sh
@@ -4,5 +4,19 @@ echo "hello"
 # This is a comment
 echo "there"
 
+#echo prints text to standard output, but can be directed elsewhere
+
 #chmod +x myscript.sh
 #./myscript.sh
+worldsize=big
+echo hello $worldsize world
+echo "The kernel is $(uname -r)"
+
+#Quote marks. 
+#No quotes -> bash reads along as it makes sense ex:escaping character
+#Single quotes -> everthing inside is literal, no substitution, etc
+#double quotes -> bash still interprets substitutions, expansions, evaluations, varibales, etc.
+
+echo 
+echo "more space!"
+echo
